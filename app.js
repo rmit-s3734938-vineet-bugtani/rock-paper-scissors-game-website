7f0833ada43d679202981f23b5de7369b7ab758d
Vineet <s3734938@student.rmit.edu.au>
2019-12-20 13:23:16 +0530

    Finished app with js code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore = 0;
+var userScore = 0;
+var computerScore = 0;
 const userscore_span = document.getElementById("user-score");
 const computerscore_span = document.getElementById("computer-score");
 const scoreboard_div = document.querySelector(".score-board");
@@ -7,7 +7,70 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissor_div = document.getElementById("s");
+const choices = ["r", "p", "s"];
 
-rock_div.addEventListener("click", function(){ console.log("Rock clicked"); });
-paper_div.addEventListener("click", function(){ console.log("Paper clicked"); });
-scissor_div.addEventListener("click", function(){ console.log("Scissor clicked"); });
+var userChoice;
+var computerChoice;
+
+rock_div.addEventListener("click", function(){
+    game("r")
+});
+paper_div.addEventListener("click", function(){
+    game("p")
+});
+scissor_div.addEventListener("click", function(){
+    game("s")
+});
+
+function getComputerChoices() {
+    const numericalChoice = Math.floor((Math.random() * 3));
+    return choices[numericalChoice];
+}
+
+function win(){
+    console.log("User wins");
+    userScore++;
+    userscore_span.innerHTML = userScore;
+    result_div.innerHTML = symbolConverter(this.userChoice) + " beats " + symbolConverter(this.computerChoice) + ".You win!!"
+}
+
+function symbolConverter(symbol){
+    if(symbol === "p") return "Paper"
+    else if (symbol === "r") return "Rock"
+    else return "Scissors"
+}
+
+function draw(){
+    console.log("Draw");
+    result_div.innerHTML = "Both chose " + symbolConverter(this.computerChoice) + ".Its a draw!!"
+}
+
+function lose(){
+    console.log("User loses");
+    computerScore++;
+    computerscore_span.innerHTML = computerScore;
+    result_div.innerHTML = symbolConverter(this.computerChoice) + " beats " + symbolConverter(this.userChoice) + ".You lose!!"
+}
+
+function game(userChoice) {
+    this.userChoice = userChoice;
+    this.computerChoice = getComputerChoices();
+    var n = computerChoice.localeCompare(this.userChoice);
+    console.log(computerChoice);
+    console.log(this.userChoice);
+    if(n == 0){
+        draw();
+    }
+    else{
+        switch (this.userChoice + computerChoice) {
+            case "rs":
+            case "sp":
+            case "pr":
+              win();
+              break;  
+            default: 
+              lose();
+          }
+    }
+  
+}
